4db97488dc9c3b2d279badac00961b1358e8f4dc
Carlos Harim Pérez Rodríguez <harim@Carloss-MacBook-Pro-2.local>
2022-03-23 19:05:02 -0600

    App finished

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import { useEffect, useState } from "react";
+import { SearchComponent } from "./components/Search/search.component";
+import { GifContainer } from "./components/GifContainer/gifContainer.component";
+import { getData } from "./services/api.service";
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello World!</div>;
+  const [value, setValue] = useState("");
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    getData(value).then((data) => setData(data.data));
+  }, [value]);
+
+  return (
+    <div className="App">
+      <SearchComponent value={value} setValue={setValue} />
+      <GifContainer data={data} />
+    </div>
+  );
 }
 
 export default App;
